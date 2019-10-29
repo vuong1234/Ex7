979e1556dac4db276a0dd3f1b5937dafe6e21bde
tankhanh <dinhtankhanh14@gmail.com>
2019-10-29 17:57:37 +0700

    vuong

---
+++ ConsoleApplication1/ConsoleApplication1/Circle1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Circle1
+    {
+        private double radius;
+        public Circle1()
+        {
+            radius = 1.0;
+        }
+        public Circle1(double radius)
+        {
+            this.radius = radius;
+        }
+        public double Getradius()
+        {
+            return this.radius;
+        }
+        public void Setradius(double radius)
+        {
+            this.radius = radius;
+        }
+        public double GetArea()
+        {
+            return this.radius * this.radius * Math.PI;
+        }
+
+        public double GetCircumference()
+        {
+            return this.radius * 2 * Math.PI;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Circle [ Radius = {0}]", radius);
+
+
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle1 c1 = new Circle1();
+            Console.WriteLine("The circle has radius of {0} and area of {1} and circumference {2}",
+                c1.Getradius(), c1.GetArea(), c1.GetCircumference());
+
+            Circle1 c2 = new Circle1(2);
+            Console.WriteLine("The circle has radius of {0} and area of {1} and circumference {2} ",
+                c2.Getradius(), c2.GetArea(), c2.GetCircumference());
+
+            Circle1 c3 = new Circle1(2);
+            c3.Setradius(5);
+            Console.WriteLine("radius is: " + c3.Getradius());
+        }
+    }
+}
